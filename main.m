ee51ba17873a6afb52769a53d4d27575aa9aff15
zwz <zwz14@outlook.com>
2018-10-29 15:29:13 +0800

    add code about store z_compensate in binary file

--- main.m
+++ main.m
@@ -6,6 +6,8 @@ real_image_name = ['realimage', name];
 z_image_name = ['z_', name];
 input_image = imread(name);
 bin_file_name = 'short_throw_depth_camera_space_projection.bin';
+z_compensate_name = 'z_compensate.bytes';
+z_c_file = fopen(z_compensate_name, 'w');
 % get real depth image
 real_image = process_image(input_image);
 imwrite(real_image, real_image_name);
@@ -14,6 +16,7 @@ bin_file = fopen(bin_file_name, 'r');
 [data_x, data_y, data_z] = process_bin(bin_file);
 data_compensate = sqrt(data_x.^2 + data_y.^2 + data_z.^2);
 data_compensate(data_compensate == Inf) = 1;
+z_c_count = fwrite(z_c_file, data_compensate(:), 'float32');
 real_image_double = double(real_image);
 z_image_double = real_image_double ./ data_compensate;
 z_image = uint16(z_image_double);
@@ -28,3 +31,4 @@ figure, imshow(output_rgb_image), impixelinfo;
 figure, imshow(r_image);
 % close all open file
 fclose(bin_file);
+fclose(z_c_file);
